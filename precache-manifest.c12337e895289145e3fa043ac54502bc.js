1a45fd8982962b0ff94e82cb378e07c6ca357ae5
kjyub <kjyub08@naver.com>
2020-06-02 00:57:41 +0900

    Updates

---
+++ precache-manifest.c12337e895289145e3fa043ac54502bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e76e4c2bc80ddcb860c01a0df6b0c9",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f18d9d4c65a7a1485b46",
+    "url": "/movie_app/static/css/main.70e109f2.chunk.css"
+  },
+  {
+    "revision": "8357a6ebb7b64d4e7f44",
+    "url": "/movie_app/static/js/2.202371d0.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/movie_app/static/js/2.202371d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18d9d4c65a7a1485b46",
+    "url": "/movie_app/static/js/main.57aa1357.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
